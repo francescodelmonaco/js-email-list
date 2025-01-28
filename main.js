6824877dcea3bc0ecffcbfdfbf6e17a096013e16
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-28 15:46:56 +0100

    add: generator button

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const mailList = document.getElementById("mailList");
+const btn = document.getElementById("btn");
 
-for (let i = 0; i < 10; i++) {
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail", { method: "GET" })
-        .then(response => response.json())
-        
-        .then(data => {
-            console.log(data.response);
-            mailList.innerHTML += `<li>${data.response}</li>`;
-        })
-        
-        .catch(error => {
-            console.error(error);
-        });
+function generator() {
+    for (let i = 0; i < 10; i++) {
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail", { method: "GET" })
+            .then(response => response.json())
+            
+            .then(data => {
+                console.log(data.response);
+                mailList.innerHTML += `<li>${data.response}</li>`;
+            })
+            
+            .catch(error => {
+                console.error(error);
+            });
+    }
 }
+
+btn.addEventListener("click", () => {
+    generator();
+})
